2d3ff613fda4c5b10a482b8325ef7193c5f115a8
Abdeljalil37 <widadpop@gmail.com>
2023-11-28 02:14:49 +0100

    made some changes and fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,19 +6,19 @@
  * @value: The value to put in the new node.
  *
  * Return: if there is an error return null,
- * 		else return a pointer to the new node.
+ *      else return a pointer to the new node.
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if(new_node == null)
-        return (null);
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->parent = parent;
+    new_node->parent = parent;
     new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-    return(new_node);
+    return (new_node);
 }

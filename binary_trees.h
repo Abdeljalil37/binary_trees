c8ecb85b0a0d0ec9c82a038f33a6b54b05902b17
Abdeljalil37 <widadpop@gmail.com>
2023-11-28 02:15:22 +0100

    delete unused func

--- binary_trees.h
+++ binary_trees.h
@@ -31,8 +31,6 @@ typedef struct binary_tree_s binary_tree_t;
 
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 

b0a5dee09c59d104318dbbd355c37c9cc667407c
Abdeljalil37 <widadpop@gmail.com>
2023-11-29 18:01:21 +0100

    rewrite the code of balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,27 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l_height = 0, r_height = 0;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
+	return (0);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
 	if (tree)
 	{
-		if (tree->left)
-			l_height = 1 + binary_tree_balance(tree->left);
-		if (tree->right)
-			r_height = 1 + binary_tree_balance(tree->right);
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
 	}
-	else
-	{
-		return (0);
-	}
-	return (l_height - r_height);
+	return (0);
 }

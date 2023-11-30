a84b0e0d0aa6fe5eaab3896c4bbf29398d4a0718
Abdeljalil37 <widadpop@gmail.com>
2023-11-30 01:12:15 +0100

    try fix error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = (binary_tree_node(parent, value));
+	binary_tree_t *new_node;
 
 	if (!parent)
 		return (NULL);
+
+	new_node = (binary_tree_node(parent, value));
 
 	if (!new_node)
 		return (NULL);
